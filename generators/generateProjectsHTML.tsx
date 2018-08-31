513ec742b61c47f19134dd47ffec5b852013d035
zhukovka <helen.zhukova@gmail.com>
2018-08-31 21:52:07 +0300

    generators

--- generators/generateProjectsHTML.tsx
+++ generators/generateProjectsHTML.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import {renderToString} from "react-dom/server";
 import * as fs from "fs";
-
+import {Project} from "../src/model/Project";
+import {DiffRange} from "../src/model/DiffRange";
+import DiffRangesApp from "../src/DiffRangesApp";
+import {Video} from "../src/model/Video";
 
 function htmlTemplate (title: string, reactDom: string) {
     return `
@@ -11,31 +14,53 @@ function htmlTemplate (title: string, reactDom: string) {
             <meta charset="utf-8">
             <title>${ title }</title>
         </head>
-        
+
         <body>
-            <div id="app">${ reactDom }</div>
+            <div id="app1">${ reactDom }</div>
             <script src="/dist/project.bundle.js"></script>
         </body>
         </html>
     `;
 }
 
-function loadRanges (sourceId: string, comparedId: string) {
-    const regex = new RegExp(`^ranges_${comparedId}.*\\.js$`);
+function readRangesJson (sourceId: string, comparedMov: string): DiffRange[] {
+    const regex = new RegExp(`^ranges_${comparedMov}.*\\.js$`);
     // @ts-ignore
     const rangesFile = fs.readdirSync(`./projects/mrestore-projects/${sourceId}`).find(file => {
         return regex.test(file);
     });
     const ranges = fs.readFileSync(`./projects/mrestore-projects/${sourceId}/${rangesFile}`, 'utf8');
+    return JSON.parse(ranges);
 }
-function generateHTML (projectId: string) {
-    const projectString = fs.readFileSync(`projects/mrestore-projects/${projectId}/project.js`, 'utf8');
-    const project = JSON.parse(projectString);
-    const app = renderToString(<div>{projectId}</div>);
-    return htmlTemplate(projectId, app);
+
+function readProjectJson (projectId: string): Project {
+    const project = fs.readFileSync(`projects/mrestore-projects/${projectId}/project.js`, 'utf8');
+    return JSON.parse(project);
+}
+
+function writeRangesHTML (sourceId: string, html: string) {
+    fs.writeFileSync(`./projects/mrestore-projects/${sourceId}/index.html`, html, 'utf8');
+}
+
+function readVideoJson (mov: string): Video {
+    const videoJs = fs.readFileSync(`projects/storage/${mov}/video.js`, 'utf8');
+    return JSON.parse(videoJs);
+}
+
+function generateHTML (projectId: string, comparedMov: string) {
+    const project = readProjectJson(projectId);
+    const sourceMov = project.masterId;
+    const sourceVideo = readVideoJson(sourceMov);
+    const comparedVideo = readVideoJson(comparedMov);
+    const app = renderToString(<DiffRangesApp ranges={readRangesJson(projectId, comparedMov)}
+                                              sourceVideo={sourceVideo}
+                                              comparedVideo={comparedVideo}/>);
+    const html = htmlTemplate(projectId, app);
+    writeRangesHTML(projectId, html);
+    return `./projects/mrestore-projects/${projectId}/index.html`;
 }
 
 
 const args = process.argv;
-console.log(generateHTML(args[2]));
+console.log(generateHTML(args[2], args[3]));
 
