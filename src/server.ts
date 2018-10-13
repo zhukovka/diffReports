4e3cf5ce5251f3efc367e864a360098c65c86fd1
zhukovka <helen.zhukova@gmail.com>
2018-10-13 22:25:05 +0300

    server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express = require("express");
+import graphqlHTTP = require("express-graphql");
+import {buildSchema} from "graphql";
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {hello : () => 'Hello world!'};
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema : schema,
+    rootValue : root,
+    graphiql : true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
